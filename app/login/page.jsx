f47542ba15fe26c7b8ae98480a7647eff11739b6
Mazaharul Islam <159911942+mazaharul2k24@users.noreply.github.com>
2024-07-03 20:04:15 +0600

    99page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function page() {
-  
+  const rn=Math.random()
   return (
-    <div>This is Random number {Math.random()}</div>
+    <div>This is Random number {rn}</div>
   )
 }
